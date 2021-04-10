8a04d084734023fbb3507ad7cc29cbd2d5e400a6
Adam Nitti <adamnitti@gmail.com>
2021-04-10 12:20:32 -0500

    Week 1 Workshop Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -11,14 +11,34 @@ campsiteRouter.route('/')
     res.end('Will send all the campsites to you');
 })
 .post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
 })
 .put((req, res) => {
-    res.statusCode= 403;
+    res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
 .delete((req, res) => {
     res.end('Deleting all campsites');
 });
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send campsite to you with Id: ${req.params.campsiteId}`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/campsiteId`);
+})
+.put((req, res) => {
+    res.end(`Will modify campsite name: ${req.body.name} description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Will delete campsite ${req.params.campsiteId}`);
+})
 
 module.exports = campsiteRouter;
